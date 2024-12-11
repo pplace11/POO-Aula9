d8fa830ff46a8b91845dcfc5f6273abf89ac74a1
pplace11 <pedro.place@hotmail.com>
2024-12-11 16:56:43 +0000

    Create Retangulo.java

---
+++ Geometria/Retangulo.java
@@ -0,0 +1,24 @@
+package Geometria;
+
+public class Retangulo implements Medidas {
+    private double base;
+    private double altura;
+
+    public Retangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+
+    public double calcularVolume() {
+        return 0;
+    }
+    
+}
