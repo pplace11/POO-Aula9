4b0aef6ccad9173adb191ec5623ea983e68f67d4
pplace11 <pedro.place@hotmail.com>
2024-12-11 16:56:53 +0000

    Create Circulo.java

---
+++ Geometria/Circulo.java
@@ -0,0 +1,22 @@
+package Geometria;
+
+public class Circulo implements Medidas {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    public double calcularVolume() {
+        return 0;
+    }
+    
+}
